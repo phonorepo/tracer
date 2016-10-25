deaad46066f960024b7a4b757c5720fa4202443a
phonorepo <phono@keemail.me>
2016-10-25 18:36:57 +0200

    added testdummy project
    for debugging

---
+++ testdummy/Windows/About.xaml.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace testdummy.Windows
+{
+    public partial class About : Window
+    {
+        public About()
+        {
+            InitializeComponent();
+        }
+
+        private void About1_Closed(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
